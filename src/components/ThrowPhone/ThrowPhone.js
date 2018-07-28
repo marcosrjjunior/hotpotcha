d25d68f7bc6602408d27fc5014aef3280f93fe50
jordanhennell <jordan.hennell@hotmail.com>
2018-07-28 16:46:32 +1000

    Navigate past ThrowPhone when phone is thrown
    Still allow manual navigation using link

--- src/components/ThrowPhone/ThrowPhone.js
+++ src/components/ThrowPhone/ThrowPhone.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { afterThrown } from "../../services/motion";
 import "./ThrowPhone.scss";
 
-export class ThrowPhone extends Component {
+class ThrowPhone extends Component {
     render() {
+        afterThrown(() => this.props.history.push("ask-rhyme"))
         return <div>
             <p>Throw the phone</p>
-            <Link to="ask-rhyme">Ask Rhyme</Link>
+            <Link to="ask-rhyme">Ask Rhyme</Link> {/* allow manual navigation too */}
         </div>
     }
 }
+
+export default withRouter(ThrowPhone);
