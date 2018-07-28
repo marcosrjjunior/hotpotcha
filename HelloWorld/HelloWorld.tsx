a8151c14f655db2a46928bbba2653a1348124354
jordanhennell <jordan.hennell@hotmail.com>
2018-07-28 11:13:10 +1000

    Wireframe for state management using React state

--- HelloWorld/HelloWorld.tsx
+++ HelloWorld/HelloWorld.tsx
@@ -1,3 +1,20 @@
 import React = require("react");
 
-export const HelloWorld = () => <div>Hello World!</div>;
+export class HelloWorld extends React.Component<{}, { model: HelloWorldModel }> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { model: new HelloWorldModel() };
+    }
+
+    render() {
+        return <>
+            <div>Hello world</div>
+            <div>Count: {this.state.model.count}</div>
+            <button onClick={() => this.setState({ model: { count: ++this.state.model.count } })}>Increment</button>
+        </>
+    }
+}
+
+class HelloWorldModel {
+    count = 1;
+}
