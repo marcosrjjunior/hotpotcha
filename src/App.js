2cc31f57a314080d4ab37e5bf679788970a67e07
marcosrjjunior <marcosrjjunior@gmail.com>
2018-07-28 01:06:02 -0300

    Adding react router and tweaks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Start from './components/Start/Start.js';
-import './App.css';
+import SelectPlayers from './components/SelectPlayers/SelectPlayers.js';
 import './App.scss';
-import './App.less';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Start />
-          <span className="sassClass">Sass</span><br />
-          <span className="lessClass">Less</span>
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+               <Router>
+                    <div>
+                        <Route exact path="/" component={Start}/>
+                        <Route path="/select-players" component={SelectPlayers}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
