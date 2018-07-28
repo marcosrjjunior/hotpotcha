7a6c18e0c4d7a4f58ad9c79121760036930a970b
jordanhennell <jordan.hennell@hotmail.com>
2018-07-28 19:17:27 +1000

    Speech input for player name

--- src/components/PlayerName/PlayerName.js
+++ src/components/PlayerName/PlayerName.js
@@ -1,12 +1,19 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import SpeechRecognition from "react-speech-recognition";
 import "./PlayerName.scss";
 
-export class PlayerName extends Component {
+class PlayerName extends Component {
     render() {
+        let { transcript, startListening, stopListening } = this.props;
+
         return <div>
-            <p>Player name: <input type="text" /></p>
+            <p>Player name {this.transcript}</p>
             <Link to="/rules">Rules</Link>
+            <button onMouseDown={startListening} onMouseUp={stopListening}>Listen</button>
+            <div>{transcript}</div>
         </div>;
     }
 }
+
+export default SpeechRecognition(PlayerName);
