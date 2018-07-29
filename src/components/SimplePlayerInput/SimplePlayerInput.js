c2e1e34ae79aa916cdd09b804ed4ffb35af76a0e
marcosrjjunior <marcosrjjunior@gmail.com>
2018-07-28 23:29:23 -0300

    Limit the player tweak

--- src/components/SimplePlayerInput/SimplePlayerInput.js
+++ src/components/SimplePlayerInput/SimplePlayerInput.js
@@ -7,26 +7,48 @@ import { TitleText } from "../Utils/TitleText";
 export class SimplePlayerInput extends Component {
     constructor() {
         super();
-        this.state = { inputValues: ["", ""] };
+        this.state = { inputValues: [""] };
     }
 
     render() {
-        return <div>
-            <TitleText>Add Your Team Mates</TitleText>
-            {this.state.inputValues.map((value, i) =>
-                <TextBox key={i}
-                    value={value}
-                    onChange={e => this.updateInputValue(e.target.value, i)}
-                    placeholder={`Enter Player ${i + 1}`} />
-            )}
-            <LinkButton to="rules" onClick={() => this.props.onPlayersSet(this.nonEmptyInputValues)}>Rules</LinkButton>
-        </div>
+        let nextButton = '';
+        if (this.state.inputValues.length > 2) {
+            nextButton = <LinkButton to="rules" 
+                    onClick={() => this.props.onPlayersSet(this.nonEmptyInputValues)}>Next</LinkButton>;
+        }
+
+        return (
+            <div className="SimplePlayerInput">
+                <TitleText className="title">Add Your Team Mates</TitleText>
+                {this.state.inputValues.map((value, i) =>
+                    <TextBox key={i}
+                        value={value}
+                        onChange={e => this.updateInputValue(e.target.value, i)}
+                        placeholder={`Enter Player ${i + 1}`} />
+                )}
+                {nextButton}
+                
+            </div>
+        )
     }
 
     updateInputValue(newValue, inputIndex) {
         let inputValues = this.state.inputValues;
         inputValues[inputIndex] = newValue;
-        this.setState({ inputValues: [...inputValues, ""] });
+        this.setState({ inputValues: [...inputValues] }); 
+
+        let lastItemKey = inputValues.length-1;
+        let lastItem = inputValues[lastItemKey];
+
+        if (lastItem.length > 0) {
+            this.setState({
+                inputValues: [...inputValues, ""]
+            }, () => {
+                if (lastItem.length > 0) {
+                    this.setState({ inputValues: [...inputValues, ""] });
+                }
+            })
+        }
     }
 
     get nonEmptyInputValues() {
