b8ef21a026834db277d0d594dda92f6ace971f4c
jordanhennell <jordan.hennell@hotmail.com>
2018-07-29 12:41:46 +1000

    Check if word rhymes in GameStart

--- src/components/GameStart/GameStart.js
+++ src/components/GameStart/GameStart.js
@@ -8,11 +8,9 @@ import Incorrect from './../Utils/Incorrect.js';
 class GameStart extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             word: '',
-            error: '',
-            answer: '',
+            error: false,
         };
     }
 
@@ -20,36 +18,36 @@ class GameStart extends Component {
         this.props.history.push('game-over');
     }
 
-    request(word) {
-        console.log('request function word: ', word)
+    async request(word) {
         if (word) {
-            // send this logic to a service?
-            let { words } = this.props;
-            if (!words) return;
-            let position = words.map(function(o) { return o.word; }).indexOf(word);
-            if (position !== -1) {
-                this.setState({ answer: words[position].word });
+            // TODO send this logic to a service
+            let rhymingWords = await this.props.rhymingWords;
+            if (!rhymingWords) return;
+            let match = rhymingWords.find(w => w.word === word);
 
-                // Throw to next person;
-                // Check players array
-            } else {
-                // Wrong answer
-                this.setState({ error: "ohh noo, we don't have this word, keep trying" });
-            }
+            if (match) {
+                this.setState({ error: false });
+                this.props.onAnswer(match.word);
+                this.props.onChangePlayer();
+                setTimeout(() => this.props.history.push('throw-phone'), 1000);
+            } else
+                this.setState({ error: true });
+     
 
             return;
         }
 
-        this.setState({ error: "ohh noo, we don't have this word, keep trying" });
+        this.setState({ error: true });
     }
 
     render() {
         return (
             <div className="GameStart">
                 <Countdown time={15} end={() => this.gameOver()} />
-                <p>{this.answer}</p>
-                <Incorrect message={this.error} />
-                <Microphone request={(word) => this.request(word)}/>
+                <p>{this.props.mostRecentAnswer}</p>
+                {this.state.error &&
+                    <Incorrect message={"ohh noo, we don't have this word, keep trying"} />}
+                <Microphone request={word => this.request(word)} />
             </div>
         )
     }
