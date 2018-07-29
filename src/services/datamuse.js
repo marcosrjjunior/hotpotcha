38aad030106449ee45dfd739315ad1ee61f8990f
jordanhennell <jordan.hennell@hotmail.com>
2018-07-29 10:53:35 +1000

    Increase max retrieved rhymes to 1000

--- src/services/datamuse.js
+++ src/services/datamuse.js
@@ -1,7 +1,7 @@
-const datamuseApiEndpoint = "https://api.datamuse.com/words";
+const datamuseApiEndpoint = "https://api.datamuse.com/words?max=1000&";
 
-export const getRhyme = async (word) => await get(`rel_rhy=${word}`)
+export const getRhyme = async word => await get(`rel_rhy=${word}`)
 
-export const get = async (queryParams) =>  await fetch(`${datamuseApiEndpoint}?${queryParams}`)
+export const get = async queryParams => await fetch(`${datamuseApiEndpoint}${queryParams}`)
     .then(response => response.json());
 
